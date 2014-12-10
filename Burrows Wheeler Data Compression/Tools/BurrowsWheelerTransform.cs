af8bf437c3a8ee4fbcb7a3c73adb48d3ed6c1353
Ali Essam <ali.essam.arafa@gmail.com>
2014-12-11 00:13:12 +0200

    Insert Missing Bytes in BW Transform
    Added missing I that points to S0

--- Burrows Wheeler Data Compression/Tools/BurrowsWheelerTransform.cs
+++ Burrows Wheeler Data Compression/Tools/BurrowsWheelerTransform.cs
@@ -10,23 +10,32 @@ namespace Burrows_Wheeler_Data_Compression.Tools
     {
         public static byte[] Transform(byte[] input)
         {
-            byte[] output = new byte[input.Length];
+            byte[] output = new byte[input.Length + 4];
             List<RotatableByte> robs = new List<RotatableByte>(input.Length);
             for (int i = 0; i < input.Length; i++)
             {
                 robs.Add(new RotatableByte(input, i));
             }
+            RotatableByte S0 = robs.ElementAt(0);
             robs.Sort();
             for (int i = 0; i < input.Length; i++)
             {
                 output[i] = robs.ElementAt(i).At(input.Length - 1);
             }
+            int I = robs.IndexOf(S0);
+            byte[] ibyte = IntToByteArr(I);
+            ibyte.CopyTo(output, input.Length);
             return output;
         }
 
         public static byte[] InverseTransform(byte[] input)
         {
             throw new NotImplementedException();
+        }
+
+        private static byte[] IntToByteArr(int i)
+        {
+            return BitConverter.GetBytes(i);
         }
     }
 }
--- BwdcTests/BurrowsWheelerTransformTest.cs
+++ BwdcTests/BurrowsWheelerTransformTest.cs
@@ -9,12 +9,22 @@ namespace BwdcTests
     {
         private static readonly String STR = "this is a test.";
         private static readonly String STR_TRANSFORMED = "ssat tt hiies .";
+        private static readonly byte[] IBytes = new byte[] { 14, 0, 0, 0 };
+        private static readonly byte[] STR_TRANSFORMED_BYTEARR = new byte[STR_TRANSFORMED.Length + 4];
+
+        [TestInitialize]
+        public void Init()
+        {
+            byte[] output = StringToByteArr(STR_TRANSFORMED);
+            output.CopyTo(STR_TRANSFORMED_BYTEARR, 0);
+            IBytes.CopyTo(STR_TRANSFORMED_BYTEARR, STR_TRANSFORMED.Length);
+        }
 
         [TestMethod]
         public void TestTransform()
         {
             byte[] output = BurrowsWheelerTransform.Transform(StringToByteArr(STR));
-            Assert.AreEqual<String>(STR_TRANSFORMED, ByteArrToString(output));
+            Assert.AreEqual<String>(ByteArrToString(STR_TRANSFORMED_BYTEARR), ByteArrToString(output));
         }
 
         [TestMethod]
